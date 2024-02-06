67d94a1b203833c77aa6901a551afb9a6db45df5
Brian Yrgon <yegonkimutai.99@gmail.com>
2024-02-06 18:58:24 +0300

    :sparkles: Create component layouts

---
+++ comp/resources/views/frontend/index.blade.php
@@ -0,0 +1,18 @@
+<x-web-layout>
+    <x-slot:title>
+        Custom Components
+    </x-slot>
+
+    <div class='py-5'>
+        <div class="container">
+            <h3> Welcome to Index page </h3>
+        </div>
+    </div>
+
+    <x-slot:scripts>
+        <script>
+            console.log('This is my script area')
+        </script>
+    </x-slot>
+
+</x-web-layout>
