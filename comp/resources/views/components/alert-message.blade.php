1bb80cb593338f62c646258e1f50c227a5cea8e6
Brian Yrgon <yegonkimutai.99@gmail.com>
2024-02-08 11:50:57 +0300

    :sparkles: Add props to the components

--- comp/resources/views/components/alert-message.blade.php
+++ comp/resources/views/components/alert-message.blade.php
@@ -1,4 +1,5 @@
-<div>
-    <!-- Simplicity is the consequence of refined emotions. - Jean D'Alembert -->
-    <h2>Hello!! I am alert message component</h2>
+@props(['message'])
+
+<div class='alert alert-success'>
+    <h5>{{ $message }}</h5>
 </div>
--- comp/resources/views/frontend/index.blade.php
+++ comp/resources/views/frontend/index.blade.php
@@ -3,20 +3,21 @@
         Custom Components
     </x-slot>
 
+    @php
+        $successMessage = 'Saved Successfully';
+    @endphp
 
     <div class='py-5'>
         <div class="container">
-            
-            <x-alert-message />
-            
+            <x-alert-message :message="$successMessage" />
+
             <h3> Welcome to Index page </h3>
         </div>
     </div>
 
     <x-slot:scripts>
         <script>
-            console.log('This is my script area')
+            console.log('This is my script area');
         </script>
     </x-slot>
-
 </x-web-layout>
